a64022ec7531136f57e207323305d4b910e5ff1f
tinyrick-sys <winglele@protonmail.com>
2025-02-08 16:56:36 -0500

    fixed node

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,6 +1,6 @@
 resource "proxmox_virtual_environment_download_file" "cloud_image_jammy_0_1_12" {
   content_type = "iso"
   datastore_id = "local"
-  node_name = "pve"
+  node_name = "proxmox"
   url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.12/cloud-image-x86-64-jammy-0.1.12.img"
 }
