e45f88da4045598e7074692433d5133f8277e394
tinyrick-sys <winglele@protonmail.com>
2025-02-08 16:51:30 -0500

    updating main tf

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,33 +1,6 @@
-terraform {
-  required_version = ">= 0.12"
-}
-
-output "ssh_private_key" {
-  value = "${var.SSH_PRIVATE_KEY}"
-}
-
-output "endpoint" {
-  value = "${var.PROXMOX_VE_ENDPOINT}"
-}
-
-output "password" {
-  value = "${var.PROXMOX_VE_PASSWORD}"
-}
-
-output "username" {
-  value = "${var.PROXMOX_VE_USERNAME}"
-}
-
-output "ssh_node_address" {
-  value = "${var.PROXMOX_VE_SSH_NODE_ADDRESS}"
-}
-
-output "ssh_node_name" {
-  value = "${var.PROXMOX_VE_SSH_NODE_NAME}"
-}
-
-data "proxmox_virtual_environment_vms" "vms" {}
-
-output "vms" {
-  value = data.proxmox_virtual_environment_vms.vms
+resource "proxmox_virtual_environment_download_file" "cloud_image_jammy_0_1_12" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name = "pve"
+  url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.12/cloud-image-x86-64-jammy-0.1.12.img"
 }
